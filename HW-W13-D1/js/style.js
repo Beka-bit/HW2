18ba68763d0335b883fc00e4bf17ac79c41c7240
Bekarys <b6134991@gmail.com>
2024-11-26 21:52:42 +0600

    first commit

---
+++ HW-W13-D1/js/style.js
@@ -0,0 +1,13 @@
+let players = ["Роналду","Зидан","Неймар","Бензема"]
+players[1]="Messi"
+players[4]="Ronaldinho"
+players[5]="Pushkash"
+players[6]="Pepe"
+
+    if(players.length<5){
+        console.log(`Oiunshular 5ten az ${players}`);
+      
+    }else if(players.length>5){
+        console.log(`oiunshilar 5ten kop ${players}`);
+        
+    }
