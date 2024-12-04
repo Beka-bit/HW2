37580441f6edf8b9a5f8e5fc28432e7c7ad60dd1
Bekarys <b6134991@gmail.com>
2024-12-04 23:06:15 +0600

    HW-W14-D3

---
+++ HW-W14-D3/js/style.js
@@ -0,0 +1,31 @@
+//task 1
+
+
+// let studens =[
+//     {
+//         name:"Aibek",
+//         math:85,
+//         city:"Amaty",
+//         country:"Kazakhstan"
+//     },
+//     {
+//         name:"Alina",
+//         math:92,
+//         city:"Astana"
+//     }
+// ]
+
+// let [studen1,studen2]=studens;
+// let {name:name1,math:math1,city:city1,country:country1="Kazakhstan"}=studen1;
+// let {name:name2,math:math2,city:city2,country:country2="Kazakhstan"}=studen2;
+// console.log(`Okyshi 1: ${name1} \n Math bagasy: ${math1} \n Kalasy: ${city1} \n Eli: ${country1}`);
+// console.log(`Okyshi 2: ${name2} \n Math bagasy: ${math2} \n Kalasy: ${city2} \n Eli: ${country2}`);
+
+
+// task2
+let clas1= ["Amina", "Adil", "Alina"];
+let clas2=["Berik", "Bakit", "Baian"];
+
+let qosu=["Director", ...clas1, ...clas2, "Mygalim"];
+
+console.log(qosu);
