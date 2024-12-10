8b8ce0fde62a79346752902a278f7c56403582fc
Bekarys <b6134991@gmail.com>
2024-12-10 18:30:29 +0600

    HW-W15-D2

---
+++ HW=W15-D1/js/style.js
@@ -0,0 +1,18 @@
+// 1-task       
+let taqyryp = document.querySelector('#new-title');  
+taqyryp.innerHTML = "Homework Day1 DOM";  
+
+// 2-task
+let aqparat = document.getElementById('new-past');  
+aqparat.textContent = "Ozim jaily:";  
+
+// 3-task
+let mazmun = document.getElementById('new-contend');  
+mazmun.textContent = "Berdakmet Bekarys";  
+
+// 4-task
+let hobbies = document.getElementsByClassName("hobby");
+
+hobbies[0].innerHTML = "<b>Skiing</b>";
+hobbies[1].innerHTML = "<b>Reaading</b>";
+hobbies[2].innerHTML = "<b>Swimming</b>";
